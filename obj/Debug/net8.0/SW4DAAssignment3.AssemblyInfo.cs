889066e3b73cf34f46d10500c607c1c7f0081ca5
Ehabben10 <202107666@post.au.dk>
2024-03-07 10:14:22 +0100

    adding the packages

--- obj/Debug/net8.0/SW4DAAssignment3.AssemblyInfo.cs
+++ obj/Debug/net8.0/SW4DAAssignment3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SW4DAAssignment3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90014b1143cf3c6edb1a38d68e697c249288462a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb75ad04f35e1afb4e2d00402d82df05646b4083")]
 [assembly: System.Reflection.AssemblyProductAttribute("SW4DAAssignment3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SW4DAAssignment3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
