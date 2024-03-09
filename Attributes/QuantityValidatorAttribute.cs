e7397f681d6940fc85f4246b2d0db948b2bbc9f5
Ehabben10 <202107666@post.au.dk>
2024-03-09 09:02:05 +0100

    adding opgave c

---
+++ Attributes/QuantityValidatorAttribute.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+public class QuantityValidatorAttribute : ValidationAttribute
+{
+    public QuantityValidatorAttribute() : base("Quantity must be a non-negative value.")
+    {
+    }
+    protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value != null)
+        {
+            if (value is int quantity && quantity < 0)
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+            else if (value is decimal decimalQuantity && decimalQuantity < 0)
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+        }
+
+        return ValidationResult.Success;
+    }
+}
--- obj/Debug/net8.0/SW4DAAssignment3.AssemblyInfo.cs
+++ obj/Debug/net8.0/SW4DAAssignment3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SW4DAAssignment3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba4395623c55e8c0cfb94ebfac8d119ee2c066f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22b7ae023c5171ab19d075974d9232c6a933614e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SW4DAAssignment3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SW4DAAssignment3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
