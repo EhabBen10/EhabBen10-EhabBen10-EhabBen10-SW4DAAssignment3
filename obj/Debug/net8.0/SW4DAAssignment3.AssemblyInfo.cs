4695da0c4f516bc90f3817a293de6c9657a5d466
Ehabben10 <202107666@post.au.dk>
2024-03-14 11:09:25 +0100

    removing weatherforcast

--- obj/Debug/net8.0/SW4DAAssignment3.AssemblyInfo.cs
+++ obj/Debug/net8.0/SW4DAAssignment3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SW4DAAssignment3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b59a18ef8bbf05abe6e228ffbef06c84d652d0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03ee2dd94b3ffa8a66fec2155a841cc7d4cdf53a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SW4DAAssignment3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SW4DAAssignment3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
