3f5fdf6a79861a31ad7e3327c00048df340be4ac
Ehabben10 <202107666@post.au.dk>
2024-03-14 09:50:23 +0100

    chaning branch

--- obj/Debug/net8.0/SW4DAAssignment3.AssemblyInfo.cs
+++ obj/Debug/net8.0/SW4DAAssignment3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SW4DAAssignment3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22b7ae023c5171ab19d075974d9232c6a933614e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+853fd2cc75e48b9dfacd7e422ec43f07d57be9d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SW4DAAssignment3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SW4DAAssignment3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
